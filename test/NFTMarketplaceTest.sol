e1b23b2a2d2e89c77cbb4e861b00334f53a87b56
Dinesh Ravishankar <45628943+dinesh3456@users.noreply.github.com>
2024-04-10 19:26:35 +0530

    Update NFTMarketplaceTest.sol

--- test/NFTMarketplaceTest.sol
+++ test/NFTMarketplaceTest.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {NFTMarketplace} from "../src/NFTMarketplace.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import {NFTMarketplaceProxy} from "../src/ProxyNFTMarketplace.sol";
+import {NFTMarketplaceScript} from "../script/NFTMarketplace.s.sol";
 
 
-contract NFTMarketplaceTest is Test, ERC721Holder {
-    NFTMarketplace marketplace;
+contract NFTMarketplaceTest is Test {
+
+     // Define a variable to store the proxy address
+    address public proxyAddress;
+    NFTMarketplaceScript public script;
+    NFTMarketplace public marketplace;
     address payable owner;
     uint256 _tokenId = 1;
     uint256 _price = 1 ether;
@@ -15,7 +20,9 @@ contract NFTMarketplaceTest is Test, ERC721Holder {
 
     function setUp() public {
         owner = payable(address(this));
-        marketplace = new NFTMarketplace("NFTMarketplace", "NFTM");
+        marketplace = new NFTMarketplace();
+        script = new NFTMarketplaceScript();        
+        proxyAddress = script.run();
         marketplace.assignCreatorRole(owner);
         marketplace.assignBuyerRole(owner);
         marketplace.assignSellerRole(owner);
@@ -24,10 +31,20 @@ contract NFTMarketplaceTest is Test, ERC721Holder {
         marketplace.createAndListToken("https://tokenURI", 1 ether);
         marketplace.buyNFT{value: 1 ether}(_tokenId); // Buy the token
         marketplace.setPrice(_tokenId, _price); // Set price for the token
-
     }
 
+     // Test case to check if the proxy contract was deployed successfully
+    function testProxyDeployment() public view {
+        address addressZero = address(0);
+        assertNotEq(proxyAddress, addressZero, "Proxy address can't be zero address");
+    }
 
+    // Test case to check if the proxy has the correct implementation
+    function testProxyImplementation() public view {
+        assertEq(address(marketplace), proxyAddress, "Proxy should point to NFTMarketplace implementation");
+    }   
+
+    
     function testCreateAndListToken() public view {
         assertEq(marketplace.getTokenURI(_tokenId), "https://tokenURI");
         assertEq(marketplace.nftPrices(_tokenId), 1 ether);
@@ -63,7 +80,4 @@ contract NFTMarketplaceTest is Test, ERC721Holder {
         uint256 nonexistentTokenId = 9999;
         marketplace.setPrice(nonexistentTokenId, _price);
     }
-
-
-
 }
