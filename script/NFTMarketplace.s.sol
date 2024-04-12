f8c759320f2bfd919f4d0393a08e196b33ade147
Dinesh Ravishankar <45628943+dinesh3456@users.noreply.github.com>
2024-04-12 17:26:57 +0530

    Update NFTMarketplace.s.sol

--- script/NFTMarketplace.s.sol
+++ script/NFTMarketplace.s.sol
@@ -1,23 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {NFTMarketplace} from "../src/NFTMarketplace.sol";
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract NFTMarketplaceScript is Script {
+    NFTMarketplace public marketplace;
+    NFTMarketplace public proxy;
 
-    function run() external payable returns (address){
-        address proxy = deployProxy();
+    bytes32 ADMIN_ROLE;
+    bytes32 CREATOR_ROLE;
+    bytes32 SELLER_ROLE;
+    bytes32 BUYER_ROLE;
+
+    function run() external returns (NFTMarketplace) {
+        // Deploy the NFTMarketplace contract
+        marketplace = new NFTMarketplace();
+
+        // Deploy the transparent upgradeable proxy
+        proxy = NFTMarketplace(
+            address(
+                new TransparentUpgradeableProxy(
+                    address(marketplace), msg.sender, abi.encodeWithSignature("initialize(address)", msg.sender)
+                )
+            )
+        );
+
+        ADMIN_ROLE = marketplace.ADMIN_ROLE();
+        CREATOR_ROLE = marketplace.CREATOR_ROLE();
+        SELLER_ROLE = marketplace.SELLER_ROLE();
+        BUYER_ROLE = marketplace.BUYER_ROLE();   
+
+
+        _createAndListToken("https://tokenURI", 1 ether);
+
+
         return proxy;
     }
 
-    function deployProxy() public returns (address) {
-        vm.startBroadcast();
-        NFTMarketplace marketplace = new NFTMarketplace();
-        ERC1967Proxy proxy = new ERC1967Proxy(address(marketplace),"");
-        NFTMarketplace(address(proxy)).initialize();
-        vm.stopBroadcast();
-        return address(proxy);
+    function _createAndListToken(string memory _tokenURI, uint256 price) public returns(uint256 tokenId){
+        vm.startPrank(msg.sender);
+        proxy.grantRole(CREATOR_ROLE, msg.sender);
+        vm.stopPrank();
+
+        vm.startPrank(msg.sender);
+        return proxy.createAndListToken(_tokenURI, price);
+        
     }
 }
